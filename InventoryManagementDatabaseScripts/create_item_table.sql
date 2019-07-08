2a349b7de798d29c81434b5de871c0e5941093fe
Garry-H <gerhardthilts@gmail.com>
2019-07-08 09:40:44 -0700

    modified items table

--- InventoryManagementDatabaseScripts/create_item_table.sql
+++ InventoryManagementDatabaseScripts/create_item_table.sql
@@ -3,14 +3,19 @@ CREATE TYPE ENUM_ITEM_CONDITION AS ENUM (
     'PRISTINE',
     'GOOD',
     'OK',
-    'POOR'
+    'POOR',
+    'UNKNOWN'
 );
 
 CREATE TABLE items (
-    uniqueId VARCHAR(255),
-    itemName VARCHAR(255),
-    lastMaintenance DATE,
-    serialNumber VARCHAR(255),
-    acquisitionDate DATE,
-    itemCondition ENUM_ITEM_CONDITION
+    id SERIAL PRIMARY KEY,
+    uniqueId VARCHAR(255) NOT NULL,
+    itemName VARCHAR(255) NOT NULL,
+    itemCondition ENUM_ITEM_CONDITION NOT NULL,
+    serialNumber VARCHAR(255) NOT NULL,
+    lastMaintenance DATE NOT NULL,
+    acquisitionDate DATE NOT NULL,
+    manufacturer VARCHAR(255) NOT NULL,
+    warranty BOOLEAN NOT NULL,
+    inStock BOOLEAN NOT NULL
 );
