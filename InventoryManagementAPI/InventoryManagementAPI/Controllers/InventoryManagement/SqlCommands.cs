0b7257eda03c30c107cce8c4520f88f638fb2914
Garry-H <gerhardthilts@gmail.com>
2019-07-09 03:04:08 -0700

    Added class to store Sql Commands

---
+++ InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagement/SqlCommands.cs
@@ -0,0 +1,7 @@
+namespace InventoryManagementAPI.Controllers.InventoryManagement
+{
+    internal static class SqlCommands
+    {
+        internal const string GetItems = "SELECT * FROM items";
+    }
+}
