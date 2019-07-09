544b5e56bc14c037fbea77f1c3092cc44f967175
Garry-H <gerhardthilts@gmail.com>
2019-07-09 06:22:03 -0700

    Added 'CreateItemsTable' command

--- InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagement/SqlCommands.cs
+++ InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagement/SqlCommands.cs
@@ -3,5 +3,13 @@ namespace InventoryManagementAPI.Controllers.InventoryManagement
     internal static class SqlCommands
     {
         internal const string GetItems = "SELECT * FROM items";
+
+        internal const string CreateItemsTable =
+            @"CREATE TABLE items (
+                Id SERIAL PRIMARY KEY, 
+                Uuid VARCHAR(255) UNIQUE NOT NULL, 
+                ItemCondition VARCHAR(255) NOT NULL, 
+                Holder VARCHAR(255) NOT NULL
+            )";
     }
 }
