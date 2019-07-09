e02bb4a4197f3271455064b298e310f942edca2f
Garry-H <gerhardthilts@gmail.com>
2019-07-09 06:23:44 -0700

    Made methods accessible through controller

--- InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagementController.cs
+++ InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagementController.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+using InventoryManagementAPI.Controllers.InventoryManagement;
+using InventoryManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InventoryManagementAPI.Controllers
 {
-    public class InventoryManagementController
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    
+    public class InventoryManagementController : ControllerBase
     {
+        // TODO remove hardcoded local test database, use configuration file
+        private static readonly PostgresDatabase Db = new PostgresDatabase("Host=localhost;Username=postgres;Password=ohsphinxofblackquartzjudgemyvow89031276;Database=inventory_management");
+        private static readonly InventoryManagementDll Dll = new InventoryManagementDll(Db);
+
+        [HttpGet("testDbConnection")]
+        public RequestResult TestDbConnection()
+        {
+            return Db.TestDatabaseConnection();
+        }
         
+        [HttpGet("getItems")]
+        public List<Item> GetItems()
+        {
+            return Dll.QueryItems();
+        }
     }
 }
