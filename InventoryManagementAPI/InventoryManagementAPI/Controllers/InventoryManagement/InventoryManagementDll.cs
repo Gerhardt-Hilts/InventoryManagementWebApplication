e8709311d03202454ae71fcaaeb062109844a395
Garry-H <gerhardthilts@gmail.com>
2019-07-09 06:24:48 -0700

    Added create table method, query items now maps to Item type

--- InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagement/InventoryManagementDll.cs
+++ InventoryManagementAPI/InventoryManagementAPI/Controllers/InventoryManagement/InventoryManagementDll.cs
@@ -13,15 +13,46 @@ namespace InventoryManagementAPI.Controllers.InventoryManagement
             _databaseInstance = databaseInstance;
         }
 
+        public void CreateTable()
+        {
+            try
+            {
+                _databaseInstance.OpenConnection();
+                var command = _databaseInstance.CreateCommand(SqlCommands.CreateItemsTable);
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _databaseInstance.CloseConnection();
+            }
+        }
+
         public List<Item> QueryItems()
         {
-            var command = _databaseInstance.CreateCommand(SqlCommands.GetItems);
-            var reader = command.ExecuteReader();
-            return reader.Cast<Item>().ToList();
+            var items = new List<Item>();
+            try
+            {
+                _databaseInstance.OpenConnection();
+                var command = _databaseInstance.CreateCommand(SqlCommands.GetItems);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    items.Add(new Item()
+                    {
+                        Id = reader.GetFieldValue<int>(0),
+                        Uuid = reader.GetFieldValue<string>(1),
+                        ItemCondition = reader.GetFieldValue<ItemCondition>(2),
+                        Holder = reader.GetFieldValue<string>(3)
+                    });
+                }
+            }
+            finally
+            {
+                _databaseInstance.CloseConnection();
+            }
+            return items;
         }
-        
         // TODO check if correct tables exist
         // TODO create methods for reading table
-        
     }
 }
